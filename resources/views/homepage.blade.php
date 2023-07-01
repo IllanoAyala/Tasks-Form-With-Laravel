5f7b646607a70e14a1ffea4c80fd8e3ccb773a4c
Illano Ayala <illanoferreira15@gmail.com>
2023-07-01 12:26:36 -0300

    Update

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Tarefas</title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <p>Olá Mundo!</p>
+    <h1>Lista de Tarefas</h1>
+    <div class="container">
+        <form action="homepage.blade.php"> 
+            <label for="nome">        
+                <input type="text" name="nome" id="" placeholder="Nome da Tarefa">               
+            </label>
+            <a href=""><img src="/img/icon-adicionar.png" alt="Adicionar" width="45px" height="45px" id="icon-add"></a><br>
+        </form>   
+    </div>
 </body>
 </html>
